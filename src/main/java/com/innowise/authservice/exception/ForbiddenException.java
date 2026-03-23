6ed371509afa00b8da211099f6f32ca20ab3c197
Antrsss <zgdasha2510@gmail.com>
2026-03-23 14:06:39 +0300

    pom.xml fixed (lombok issue)

--- src/main/java/com/innowise/authservice/exception/ForbiddenException.java
+++
@@ -1,9 +0,0 @@
-package com.innowise.authservice.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class ForbiddenException extends BaseServiceException {
-  public ForbiddenException(String message) {
-    super(message, HttpStatus.FORBIDDEN);
-  }
-}
